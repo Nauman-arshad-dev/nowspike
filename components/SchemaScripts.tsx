6c21c3a39436e484dd56dd076c23c280edf18bce
Nauman-arshad-dev <nauman.arshad483@gmail.com>
2025-03-22 21:30:52 +0500

    Move Script components to client components to fix sitemap script tag issue

--- components/SchemaScripts.tsx
+++ components/SchemaScripts.tsx
@@ -3,7 +3,43 @@
 
 import Script from "next/script";
 
-export default function SchemaScripts({ schemaData }: { schemaData: any }) {
+// Define the types for the schema data
+interface SchemaArticle {
+  "@type": string;
+  headline: string;
+  image: string[];
+  datePublished: string;
+  dateModified: string;
+  author: {
+    "@type": string;
+    name: string;
+  };
+  publisher: {
+    "@type": string;
+    name: string;
+    logo: {
+      "@type": string;
+      url: string;
+    };
+  };
+  description: string;
+  url: string;
+}
+
+interface SchemaData {
+  "@context": string;
+  "@type": string;
+  name: string;
+  url: string;
+  description: string;
+  mainEntityOfPage: {
+    "@type": string;
+    "@id": string;
+  };
+  article: SchemaArticle[];
+}
+
+export default function SchemaScripts({ schemaData }: { schemaData: SchemaData }) {
   return (
     <Script
       id="schema-org"
